c32e0c3c0fa3cebccaf014b96f750c160bf2f44f
BIANCARIZZO <131270519+BIANCARIZZO@users.noreply.github.com>
2023-11-10 18:34:04 +0200

    PinButtons

---
+++ Assets/scripts/ButtonScripts/PinTokoloshe.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PinTokoloshe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("2D_Tokoloshe");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
