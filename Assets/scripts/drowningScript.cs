6c7bffbfb07eb28530d2aca88ec2ac3e0dd62a96
everbeentothemoon <NostalgicWRLDWIDE@gmail.com>
2023-11-14 09:30:31 +0200

    becauseArt

--- Assets/scripts/drowningScript.cs
+++ Assets/scripts/drowningScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class drowningScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+    public int floaterCount = 1;
+    public float waterDrag = 0.99f;
+    public float waterAngularDrag = 0.5f;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+
+        float waveHeight = rideTheWaves.instance.GetWaveHeight(transform.position.x);
+
+        if (transform.position.y < waveHeight)
+        {
+             float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+             rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            rb.AddForce(displacementMultiplier * -rb.velocity * waterDrag * Time.deltaTime, ForceMode.VelocityChange);
+            rb.AddTorque(displacementMultiplier * -rb.angularVelocity * waterAngularDrag * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+       
     }
 }
---
+++ Assets/scripts/hydrationScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class hydrationScript : MonoBehaviour
+{
+    private MeshFilter meshFilter;
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();   
+    }
+
+    private void Update()
+    {
+        Vector3[] vertices = meshFilter.mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = rideTheWaves.instance.GetWaveHeight(transform.position.x + vertices[i].x);
+        }
+
+        meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.RecalculateNormals();
+    }
+}
